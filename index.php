c896415cda4e4e70e39454a0bff3560cc7addb40
BorsosIstvan <bistvanus@gmail.com>
2026-06-04 13:01:40 +0200

    index

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ $stmt->execute([$username]);
 
 <!DOCTYPE html>
 <html lang="nl">
+<script src="sw.js"></script>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
