9dadac10fbdba56488b95f67acac8647d65aecd8
BorsosIstvan <bistvanus@gmail.com>
2026-06-06 08:02:11 +0200

    sw

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const ASSETS = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icon/icon_192.png',
-  '/icon/icon_512.png'
+  '/icon/icon-192.png',
+  '/icon/icon-512.png'
 ];
 
 // Bestanden opslaan tijdens installatie
